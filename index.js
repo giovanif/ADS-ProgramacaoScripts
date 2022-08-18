92c8e273fab0de2b89a5018c518a40629454b4bb
giovani <giovani@loginteligente.com.br>
2022-08-17 21:02:05 -0300

    Inicialização do respositório

---
+++ index.js
@@ -0,0 +1,8 @@
+// Chamar a biblioteca express
+const express = require('express');
+
+// Armazenar na varável server a biblioteca express()
+const server = express();
+
+// Chamar a função listen para "escutar a porta que vamos utilizar"
+server.listen(3500);
